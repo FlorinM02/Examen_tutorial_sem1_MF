52802cf0839b50bf03c803d316daf4db2212c07c
FlorinM02 <florinmuica@yahoo.com>
2025-01-12 14:14:03 +0200

    create_table_partial

---
+++ 1_create.sql
@@ -0,0 +1,16 @@
+create table if not exists tara
+(
+	fid SERIAL primary key,
+	nume varchar(50) unique not null,
+	indicativ varchar(5) not null,
+	suprafata decimal null
+);
+
+create table if not exists judet
+(
+	fid serial primary key,
+	nume varchar(50) unique not null,
+	indicativ varchar(2) not null,
+	suprafata decimal null,
+	tara_fid integer not null
+);
