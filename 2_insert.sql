167334ee283fa86e99416d54bf4c2036ac2248bb
FlorinM02 <florinmuica@yahoo.com>
2025-01-12 14:28:11 +0200

    bulk_insert

--- 2_insert.sql
+++ 2_insert.sql
@@ -1,6 +1,11 @@
 --insert in table tara
 insert into tara (nume, indicativ) values ('Romania', 'RO');
+insert into tara (nume, indicativ) values
+	('Franta', 'FR'),
+	('Germania', 'DE'),
+	('Bulgaria', 'BG'),
+	('Moldova', 'MD');
 
 --insert in table judet
 insert into judet(nume, indicativ, tara_fid) values ('Timis', 'TM', '1'); 
-insert into judet(nume, indicativ, tara_fid) values ('Arad', 'AR', (select fid from tara where nume = 'Romania')); 
+insert into judet(nume, indicativ, tara_fid) values ('Arad', 'AR', (select fid from tara where nume = 'Romania'));
