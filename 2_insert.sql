c604145b63980bdbaff3b7de2c62dbb70707bc46
FlorinM02 <florinmuica@yahoo.com>
2025-01-12 14:24:06 +0200

    insert_tutorial_tarasijudete

---
+++ 2_insert.sql
@@ -0,0 +1,5 @@
+--insert in table tara
+insert into tara (nume, indicativ) values ('Romania', 'RO');
+
+--insert in table judet
+insert into judet(nume, indicativ, tara_fid) values ('Timis', 'TM', '1'); 
