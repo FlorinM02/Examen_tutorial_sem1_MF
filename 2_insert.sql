92a15e1a64cba5cf5970b9252efb35242b984cdd
FlorinM02 <florinmuica@yahoo.com>
2025-01-12 14:25:56 +0200

    insert_ar

--- 2_insert.sql
+++ 2_insert.sql
@@ -3,3 +3,4 @@ insert into tara (nume, indicativ) values ('Romania', 'RO');
 
 --insert in table judet
 insert into judet(nume, indicativ, tara_fid) values ('Timis', 'TM', '1'); 
+insert into judet(nume, indicativ, tara_fid) values ('Arad', 'AR', (select fid from tara where nume = 'Romania')); 
